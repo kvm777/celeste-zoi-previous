82cb342acba2b3d902c4000fff2bb30a40de199c
main <saikrishna11051998@gmail.com>
2024-07-02 10:35:34 +0530

    after header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/Images/logo.png";
 import "../styles/header.css";
+import Hamburger from 'hamburger-react'
+
+
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src={logo} alt="Celestezoi" />
+        <Link to="/"><img src={logo} alt="Celestezoi" /></Link>
+        <div className="hamburger">
+          <Hamburger toggled={isOpen} toggle={setOpen} className="hamburger"/>
+        </div> 
       </div>
       
       <div className="menu">
-        <Link to="/" className="menu-item">Home</Link>
-        <Link to="/home" className="menu-item">About Us</Link>
-        <Link to="/home" className="menu-item">Beauty</Link>
-        <Link to="/home" className="menu-item">Fashion</Link>
-        <Link to="/home" className="menu-item">Magazines</Link>
-        <Link to="/home" className="menu-item">Contact Us</Link>
-        
-        <a href="#" className="menu-item download-btn"> Download App</a>
+      <NavLink exact to="/" className="menu-item" activeClassName="active">Home</NavLink>
+      <NavLink to="/about-us" className="menu-item" activeClassName="active">About Us</NavLink>
+      <NavLink to="/beauty" className="menu-item" activeClassName="active">Beauty</NavLink>
+      <NavLink to="/fashion" className="menu-item" activeClassName="active">Fashion</NavLink>
+      <NavLink to="/magazines" className="menu-item" activeClassName="active">Magazines</NavLink>
+      <NavLink to="/contact-us" className="menu-item" activeClassName="active">Contact Us</NavLink>
+      <Link href="#" className="menu-item download-btn">Download App</Link>
 
       </div>
     </nav>
